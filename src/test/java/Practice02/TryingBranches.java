6023ac33891eb07e2bad69b771f8d6eb824b00df
ilhamialtinoren <ilhamialtinoren@gmail.com>
2023-12-26 15:17:26 -0800

    Ilhami is working on the same file

--- src/test/java/Practice02/TryingBranches.java
+++ src/test/java/Practice02/TryingBranches.java
@@ -2,10 +2,14 @@ package Practice02;
 
 public class TryingBranches {
 
+	//We are working on the same file with the other branch (IlhamiBranch02) as an IlhamiBranch01 and trying to create conflict
+	
 	public static void main(String[] args) {
 		
 		//I'm adding this comment after creating and working on IlhamiBranch02 Branch
 		System.out.println("I'm on IlhamiBranch01 Branch");
+		
+		//I'm adding this comment while I'm working on IlhamiBranch02 Branch
 
 	}
 
