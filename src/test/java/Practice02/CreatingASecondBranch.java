6ffa2bfe5afcf7286317f78ed5f50126cfe7ee79
ilhamialtinoren <ilhamialtinoren@gmail.com>
2023-12-26 14:45:59 -0800

    This commit is made while working on IlhamiBranch02 Branch

---
+++ src/test/java/Practice02/CreatingASecondBranch.java
@@ -0,0 +1,11 @@
+package Practice02;
+
+public class CreatingASecondBranch {
+
+	public static void main(String[] args) {
+		
+		System.out.println("We have created a second branch called IlhamiBranch02 and we are on that branch right now!");
+
+	}
+
+}
