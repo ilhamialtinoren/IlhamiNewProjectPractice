20703e8ce9842374610d7f4f31810b73c4470e53
ilhamialtinoren <ilhamialtinoren@gmail.com>
2023-12-26 14:13:14 -0800

    First commit when we are on our IlhamiBranch01 Branch

--- src/test/java/Practice01/WeAreFetchingAndPulling.java
+++ src/test/java/Practice01/WeAreFetchingAndPulling.java
@@ -5,6 +5,8 @@ public class WeAreFetchingAndPulling {
 	public static void main(String[] args) {
 		
 		System.out.println("Let's try fetching and pulling");
+		
+		System.out.println("We made this change when we are on IlhamiBranch01 Branch");
 
 	}
 
---
+++ src/test/java/Practice02/TryingBranches.java
@@ -0,0 +1,11 @@
+package Practice02;
+
+public class TryingBranches {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I'm on IlhamiBranch01 Branch");
+
+	}
+
+}
