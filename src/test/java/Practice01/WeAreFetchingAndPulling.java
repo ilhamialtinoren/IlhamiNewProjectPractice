04afd69ab8c8ef3721c02191e58d759476014c6e
ilhamialtinoren <ilhamialtinoren@gmail.com>
2023-12-26 15:05:37 -0800

    We are adding IlhamiScenario#1

--- src/test/java/Practice01/WeAreFetchingAndPulling.java
+++ src/test/java/Practice01/WeAreFetchingAndPulling.java
@@ -2,6 +2,8 @@ package Practice01;
 
 public class WeAreFetchingAndPulling {
 
+	//I'm working on IlhamiBranch01 Branch now
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Let's try fetching and pulling");
