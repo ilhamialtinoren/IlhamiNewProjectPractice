df8215f4e88a29b86561df96e89021da6791856c
ilhamialtinoren <ilhamialtinoren@gmail.com>
2023-12-26 13:44:23 -0800

    forth commit practice

---
+++ src/test/java/Practice01/WeAreStashing01.java
@@ -0,0 +1,10 @@
+package Practice01;
+
+public class WeAreStashing01 {
+
+	public static void main(String[] args) {
+		System.out.println("We did our first stash practice");
+
+	}
+
+}
--- src/test/java/Practice01/WeAreStashing2.java
+++ src/test/java/Practice01/WeAreStashing2.java
@@ -4,6 +4,8 @@ public class WeAreStashing2 {
 
 	public static void main(String[] args) {
 		System.out.println("We are trying to print this line");
+		
+		System.out.println("I'm making new changes");
 
 	}
 
