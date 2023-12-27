e76989e777bd6cd1aa04e884aec952b691626cd4
ilhamialtinoren <ilhamialtinoren@gmail.com>
2023-12-26 16:14:04 -0800

    trying new commit with new change

--- src/test/java/Practice01/WeAreTrying.java
+++ src/test/java/Practice01/WeAreTrying.java
@@ -5,6 +5,11 @@ public class WeAreTrying {
 	public static void main(String[] args) {
 		System.out.println("Let's print our first test line");
 
+		
+	
+		System.out.println("Now trying scenario 4 in a diff way");
+		
+		
 	}
 
 }
