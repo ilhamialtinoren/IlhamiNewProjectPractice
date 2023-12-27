909a396a3caefab83e21e0af1124df4ddfac74d9
ilhamialtinoren <ilhamialtinoren@gmail.com>
2023-12-26 16:22:08 -0800

    Last changes

--- src/test/java/Practice01/WeAreTrying.java
+++ src/test/java/Practice01/WeAreTrying.java
@@ -5,6 +5,9 @@ public class WeAreTrying {
 	public static void main(String[] args) {
 		System.out.println("Let's print our first test line");
 
+		
+		
+		System.out.println("I also change it as IlhamiBranch01 at the same line");
 	}
 
 }
